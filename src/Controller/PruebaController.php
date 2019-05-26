39e8a16469ac470920bbd94ffaf5e1d5bbad289e
carlosorts97 <carlosorts97@gmail.com>
2019-05-26 05:19:40 +0200

    Prueba Controller

--- src/Controller/PruebaController.php
+++ src/Controller/PruebaController.php
@@ -17,6 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PruebaController extends AbstractController
 {
@@ -25,17 +26,75 @@ class PruebaController extends AbstractController
      */
     public function index($category, $brand)
     {
-        if ($brand == 0){
-            $articles = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>$category));
+        $em = $this->getDoctrine()->getEntityManager();
+        $conn = $em->getConnection();
+        if ($brand == 0) {
+            $sql = '
+            SELECT name,articles.retail_date, articles.name,category,brand, AVG(sizes.price) AS price from articles
+            LEFT JOIN sizes ON articles.id_article = sizes.article
+            WHERE category=' . $category . '
+            GROUP BY articles.retail_date, articles.name,category,brand
+            ';
+            $category = $this->getDoctrine()->getRepository(Category::class)->find($category);
+            $brand=null;
         }
         else{
-            $articles = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>$category,'brand'=>$brand));
+            $sql = '
+            SELECT name,articles.retail_date,articles.name,category,brand, AVG(sizes.price) AS price 
+            From articles
+            LEFT JOIN sizes ON articles.id_article = sizes.article
+            WHERE category=' . $category . ' AND brand='.$brand.'
+            GROUP BY articles.retail_date, articles.name,category,brand
+            ';
+            $category = $this->getDoctrine()->getRepository(Category::class)->find($category);
+            $brand = $this->getDoctrine()->getRepository(Brands::class)->find($brand);
         }
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $articles= $stmt->fetchAll();
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
             'category' => $category,
-            'brand' => $brand
+            'brand' => $brand,
+        ]);
+    }
+
+    /**
+     * @Route("/prueba", name="prueba_home")
+     */
+    public function index_home()
+    {
+        $streetWear_hyped = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>['1','2','3','5']),array('idArticle'=>'DESC'),3);
+        $streetWear_last = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>['1','2','3','5']),array('retailDate'=>'DESC'),3);
+        $sneakers_hyped = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>['4']),array('idArticle'=>'DESC'),3);
+        $sneakers_last = $this->getDoctrine()->getRepository(Articles::class)->findBy(array('category'=>['4']),array('retailDate'=>'DESC'),3);
+        $tallas = $this->getDoctrine()->getRepository(Sizes::class)->findBy(array('article'=>2));
+        dump($tallas);
+        die();
+        $category = 2;
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $conn = $em->getConnection();
+        $sql = '
+        SELECT name,articles.retail_date, articles.name,category,brand, AVG(sizes.price) from articles
+        LEFT JOIN sizes ON articles.id_article = sizes.article
+        WHERE category='.$category.'
+        GROUP BY articles.retail_date, articles.name,category,brand
+        ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        $articles= $stmt->fetchAll();
+        dump($articles);
+        die();
+
+        return $this->render('home/home.html.twig', [
+            'streetWear_hyped' => $streetWear_hyped,
+            'streetWear_last' => $streetWear_last,
+            'sneakers_hyped' => $sneakers_hyped,
+            'sneakers_last' => $sneakers_last
         ]);
     }
 }
